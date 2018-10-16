8c1c41b5b0fecdc734eacf303abcc45ae2d34242
wangcheng <cheng291012@163.com>
2018-10-16 11:25:40 +0800

    first commit

---
+++ PersonalNotes/pages/index/index.js
@@ -0,0 +1,110 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    biji: []
+  },
+
+  onReady: function (res) {
+    var that = this
+    const db = wx.cloud.database({});
+    const table = db.collection("Note");
+    table.where({
+      _openid:'oFGu94gQWDqKLMRyJA2Kw66eL70g'
+    }).get({
+      success:function(res){
+        console.log(res.data),
+        that.setData({
+          biji: res.data
+        })
+      }
+    })
+  },
+
+  //事件处理函数
+  /*bindViewTap: function() {
+      wx.switchTab({
+        url: '../main/main'
+    })
+  },*/
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ PersonalNotes/pages/main/main.js
@@ -0,0 +1,115 @@
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }
+  },
+
+  saveNote: function (e) {
+    /*if (!hasUserInfo && canIUse) {
+      wx.showToast({
+        title: '请先登录',
+        icon: 'loading'
+      })
+    } else {*/
+      const db = wx.cloud.database({});
+      const table = db.collection("Note");
+      var newNote = e.detail.value.note;
+      if (newNote) {
+        table.add({
+          data: {
+            note: newNote,
+            date: new Date()
+          },
+          success: function (res) {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success'
+            })
+          }
+        });
+      } else {
+        wx.showToast({
+          title: '不能保存空笔记',
+          icon: 'loading',
+          duration: 2000
+        })
+      //}
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
